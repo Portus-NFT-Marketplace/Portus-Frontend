04fa6dea9fd837a4d20214c21a9b3e17c4587781
PitchapaSaelim <pitchapa.sae@ku.th>
2022-04-26 19:30:39 +0700

    Decorate nav bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = () => {
     const [toggleMenu, setToggleMenu] = React.useState(false);
   
     return (
-      <nav className="w-full flex md:justify-left justify-between items-left p-4">
-        <div className="md:flex-[0.5] flex-initial justify-left items-left">
+      <nav className="w-full flex md:justify-left items-left p-4">
+        <div className="md:flex-initial justify-center items-left">
           <img src={logo} alt="logo" className="w-32 cursor-pointer" />
         </div>   
 
