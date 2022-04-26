06ec8622f8464ae0031e19cc1caeafc3dc6104aa
PitchapaSaelim <pitchapa.sae@ku.th>
2022-04-26 18:16:46 +0700

    Setup Logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,16 @@
-const Navbar = () => {
-    return(
-        <h2>Navbar</h2>
-    );
-}
+import React from "react";
 
+import logo from "../../images/logo-nav.png";
+
+const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = React.useState(false);
+  
+    return (
+      <nav className="w-full flex md:justify-left justify-between items-left p-4">
+        <div className="md:flex-[0.5] flex-initial justify-left items-left">
+          <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+        </div>       
+      </nav>
+    );
+  };
 export default Navbar;
