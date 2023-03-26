5b23ef40c2a4fcdf5bb6a51d49bcd9f0b9f63df0
unknown <pitchapa.sae@ku.th>
2023-03-26 19:19:00 +0700

    feat: added function connect MetaMask to Buy button

---
+++ src/components/pages/shared/general/BuyButton.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useContext } from "react";
+import Cookies from "js-cookie";
+import { Button, Typography, Stack } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { AppContext } from "../../../../utils/AppProvider";
+
+const StyledBuyButton = styled(Button)({
+  marginTop: 50,
+  width: 150,
+  borderColor: "#E46842",
+  color: "#ffffff",
+  backgroundColor: "#E46842",
+  borderRadius: 8,
+  "&:hover": {
+    borderColor: "#BC6530",
+    backgroundColor: "#BC6530",
+  },
+});
+
+export default function BuyButton() {
+  const { userAddress, setUserAddress } = useContext(AppContext);
+
+  useEffect(() => {
+    async function checkMetamaskConnection() {
+      if (typeof window.ethereum !== "undefined") {
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        if (accounts.length > 0) {
+          setUserAddress(accounts[0]);
+          Cookies.set("userAddress", accounts[0]);
+        } else {
+          setUserAddress(null);
+
+          Cookies.remove("userAddress");
+        }
+      } else {
+        setUserAddress(null);
+        Cookies.set("userAddress", false);
+        Cookies.remove("userAddress");
+      }
+    }
+
+    checkMetamaskConnection();
+  }, []);
+
+  const connectMetamask = async () => {
+    try {
+      if (typeof window.ethereum !== "undefined") {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        setUserAddress(accounts[0]);
+        window.location.reload();
+      } else {
+        alert("Please install MetaMask to use this feature");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  if (userAddress) {
+    return (
+      // if user already connected navigate to function buyNFT (TBA)
+      <div>
+        <StyledBuyButton
+        //   onClick={buyNFT}
+        >
+          Buy
+        </StyledBuyButton>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <StyledBuyButton onClick={connectMetamask}>Buy</StyledBuyButton>
+    </>
+  );
+}
