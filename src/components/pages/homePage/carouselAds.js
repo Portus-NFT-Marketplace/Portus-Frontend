c48970c80280e2d40dbbc997d80818e5c910a5fc
unknown <pitchapa.sae@ku.th>
2023-03-28 19:45:38 +0700

    style: improve the style of carousel ads

--- src/components/pages/homePage/carouselAds.js
+++ src/components/pages/homePage/carouselAds.js
@@ -20,35 +20,35 @@ const CarouselAds = () => {
       <Slider {...settings}>
         <div>
           <img
-            className='carousel-image'
+            className='carousel-image-ads'
             src='https://via.placeholder.com/1200x345?text=Ad+1'
             alt='Ad 1'
           />
         </div>
         <div>
           <img
-            className='carousel-image'
+            className='carousel-image-ads'
             src='https://via.placeholder.com/1200x345?text=Ad+2'
             alt='Ad 2'
           />
         </div>
         <div>
           <img
-            className='carousel-image'
+            className='carousel-image-ads'
             src='https://via.placeholder.com/1200x345?text=Ad+3'
             alt='Ad 3'
           />
         </div>
         <div>
           <img
-            className='carousel-image'
+            className='carousel-image-ads'
             src='https://via.placeholder.com/1200x345?text=Ad+4'
             alt='Ad 4'
           />
         </div>
         <div>
           <img
-            className='carousel-image'
+            className='carousel-image-ads'
             src='https://via.placeholder.com/1200x345?text=Ad+5'
             alt='Ad 5'
           />
