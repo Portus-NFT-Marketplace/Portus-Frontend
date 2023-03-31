1fd568f9d78a4cade1da137af9e235aff98b5828
unknown <pitchapa.sae@ku.th>
2023-03-31 23:00:24 +0700

    refactor: removed comment code

--- src/components/pages/NFTCreatingPage/index.js
+++ src/components/pages/NFTCreatingPage/index.js
@@ -156,22 +156,6 @@ export default function CreatingArtworkForm({ isSignedIn, userToken, oauthToken 
   });
 
   const url = "https://portus-api.herokuapp.com/api/v1/artworks";
-  // const [token, setToken] = useState({});
-  // useEffect(() => {
-  //   axios
-  //     .post("https://portus-api.herokuapp.com/oauth/token", {
-  //       grant_type: "client_credentials",
-  //       client_id: `${process.env.REACT_APP_CLIENT_ID}`,
-  //       client_secret: `${process.env.REACT_APP_CLIENT_SECRET}`,
-  //     })
-  //     .then((response) => {
-  //       //get token from response
-  //       console.log(response);
-  //       setToken(response.data.access_token);
-  //       console.log(token);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, []);
 
   isSignedIn = Cookies.get("isSignedIn");
   if (isSignedIn != "true") {
